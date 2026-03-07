5a1f0eeaa6619d3af49567112c50a1ce4a5523ff
Pierce Hendrickson <pierce.hendrickson@usu.edu>
2026-03-07 02:14:25 +0000

    This is for Core 5. I have made the oliver directory and all of the related files inside of it.

---
+++ models/oliver/oliver_dim_employee.sql
@@ -0,0 +1,15 @@
+{{ config(
+    materialized = 'table',
+    schema = 'dw_oliver'
+) }}
+
+SELECT
+    employee_id AS employee_key,
+    employee_id,
+    first_name,
+    last_name,
+    email,
+    phone_number,
+    hire_date,
+    position
+FROM {{ source('oliver_landing', 'employee') }}
